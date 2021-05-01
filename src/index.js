0185748c31b6fd3a67eb81b85811c870330fda1e
mstanka <mice78.mm@gmail.com>
2021-05-01 11:16:58 +0200

    add toggle navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('.nav-link');
+
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+  console.log(nav.classList);
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+}
